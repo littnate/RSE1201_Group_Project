8135fa901253eb1d8c892a5cd4d5f425b57dbd91
Muhammad Salleh <sallehahmad197@gmail.com>
2026-09-15 17:13:21 +0800

    main upd

--- MS_1_files/main.c
+++ MS_1_files/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <SatComRelay.h>
+#include "SatComRelay.h"
 
 int main () {
-
-    printf("Test");
+    
+    printf("%");
     //VehDataType SCRelay_Signal_Veh_MoveUpNorth = {1000, 800, 200, 200, 50};
     return 0;
 }
